158f0b18ec3897ce9956935e181ec2adf7f72e9e
zxsharp <ishanshdimaniya@gmail.com>
2025-07-14 06:16:50 +0530

    Add file-meta-data

---
+++ boilerplate-project-filemetadata/index.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var cors = require('cors');
+require('dotenv').config();
+const multer  = require('multer');
+
+var app = express();
+
+app.use(cors());
+app.use('/public', express.static(process.cwd() + '/public'));
+
+const upload = multer({ dest: 'uploads/' });
+
+app.get('/', function (req, res) {
+  res.sendFile(process.cwd() + '/views/index.html');
+});
+
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res) => {
+  return res.json({
+    name: req.file.originalname,
+    type: req.file.mimetype,
+    size: req.file.size
+  })
+})
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, function () {
+  console.log('Your app is listening on port ' + port)
+});
